224df54a29d9b890c67c4889103f0b4f44fd1ba9
Severin Sverdvik <severisv@gmail.com>
2022-12-25 12:40:14 +0100

    Update publish script

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,3 @@
 dotnet restore .\SlackLogger
 dotnet pack .\SlackLogger\ /p:PackageVersion=%appveyor_build_version% --configuration Release -o ../.deploy
-.\nuget.exe push .deploy\SlackLogger.%appveyor_build_version%.nupkg -ApiKey %2 -source %3
+dotnet nuget push .deploy\SlackLogger.%appveyor_build_version%.nupkg --api-key %2 --source nuget.org
